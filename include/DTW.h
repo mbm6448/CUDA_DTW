6a787d02a60e61578591701bb3a746205cb66f18
Mohammad Moeini <65136221+mbm6448@users.noreply.github.com>
2025-04-16 20:30:11 -0400

    Update DTW.h

--- include/DTW.h
+++ include/DTW.h
@@ -1,20 +1,37 @@
-#pragma once
+#ifndef DTW_H
+#define DTW_H
 
 #include <vector>
-#include <cuda.h>
 
+/**
+ * Dynamic Time Warping (DTW) implementation using CUDA
+ * Computes the minimum distance between two time series
+ */
 class DTW {
 public:
-    DTW(int blockSize = 32);
-    ~DTW() = default;
-
+    /**
+     * Constructor
+     * @param blockSize The CUDA block size to use for computation
+     */
+    DTW(int blockSize = 16);
+    
+    /**
+     * Compute the DTW distance between two sequences
+     * @param x First sequence
+     * @param y Second sequence
+     * @return The DTW distance, or -1.0 if an error occurred
+     */
     double compute(const std::vector<double>& x, const std::vector<double>& y);
-    static std::vector<double> generateRandomSequence(int length);
+    
+    /**
+     * Generate a random sequence for testing
+     * @param length The length of the sequence
+     * @return A vector containing random doubles between 0 and 1
+     */
+    std::vector<double> generateRandomSequence(int length);
 
 private:
-    int BLOCK_SIZE;
+    int BLOCK_SIZE; // Block size for CUDA computation
 };
 
-__device__ double absoluteDifference(double x, double y);
-__global__ void dtwKernel(double *d, double *x, double *y, int n, int m, int blockSize);
-
+#endif // DTW_H
